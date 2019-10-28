73f0ca04e2e4e1b0f23dd20ed21533bc554c24ca
Nate-yu <1227908693@qq.com>
2019-10-28 20:11:10 +0800

    背包

--- src/Bag.java
+++ src/Bag.java
@@ -1,8 +1,44 @@
-package PACKAGE_NAME;
+import java.util.Iterator;
+import java.util.ListIterator;
 
 /**
  * @author Nate yu
  * @date 2019/10/28 - 19:56
  */
-public class Bag {
+public class Bag<Item> implements Iterable<Item> {
+    private Node first;
+    private class Node{
+        Item item;
+        Node next;
+    }
+
+    public void add(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public void remove() { }
+
+        @Override
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
 }
