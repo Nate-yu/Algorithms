3b9b09fe5c3504c1453c920f7b9bc91312626816
Nate-yu <1227908693@qq.com>
2019-10-29 19:15:32 +0800

    排序算法模板

--- src/Sort/Template.java
+++ src/Sort/Template.java
@@ -1,8 +1,44 @@
 package Sort;
 
+import java.util.Scanner;
+
 /**
  * @author Nate yu
  * @date 2019/10/29 - 18:37
  */
 public class Template {
+    public static void sort(Comparable[] a){
+
+    }
+
+    private static boolean less(Comparable v, Comparable w){ // 当v < w时才返回true
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exchange(Comparable[] a, int i , int j){ // 交换数据
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show (Comparable[] a){ // 在单行中打印数组
+        for (int i = 0; i < a.length; i++){
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static boolean isSorted(Comparable[] a){ // 测试数组是否有序
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i],a[i-1])) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Comparable[] a = {1,2,56,8,6,4};
+        sort(a);
+        show(a);
+    }
 }
