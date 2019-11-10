a8495b9358255d754d491fc6574ac0cfec3d6fe0
Nate-yu <1227908693@qq.com>
2019-11-10 14:49:36 +0800

    二分查找（基于有序数组））

--- src/Search/BinarySearchST.java
+++ src/Search/BinarySearchST.java
@@ -4,5 +4,96 @@ package Search;
  * @author Nate yu
  * @date 2019/11/10 - 14:13
  */
-public class BinarySearchST {
+public class BinarySearchST<Key extends Comparable<Key>,Value> {
+
+    private Key[] keys;
+    private Value[] values;
+    private int N;
+
+    public BinarySearchST(int capacity){
+        keys = (Key[]) new Comparable[capacity];
+        values = (Value[]) new Object[capacity];
+    }
+
+    public int size(){
+        return N;
+    }
+
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+
+    public int rank(Key key){
+        int low = 0,high = N-1;
+        while (low <= high){
+            int mid = low + (high-low) / 2;
+            int cmp = key.compareTo(keys[mid]);
+            if (cmp < 0) high = mid - 1;
+            else if (cmp > 0) low = mid +1;
+            else return mid;
+        }
+        return low;
+    }
+
+    public Value get(Key key){
+        if (isEmpty()){
+            return null;
+        }
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0) return values[i];
+        else return null;
+    }
+
+    public void put(Key key,Value value){ // 查找键，找到则更新值，否则创建新元素
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0) {
+            values[i] = value;
+            return;
+        }
+        for (int j = N; j > i; j--){
+            keys[j] = keys[j-1];
+            values[j] = values[j-1];
+            keys[i] = key;
+            values[i] = value;
+            N++;
+        }
+    }
+
+    public void delete(Key key){
+        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
+        if (isEmpty()) return;
+
+        // compute rank
+        int i = rank(key);
+
+        // key not in table
+        if (i == N || keys[i].compareTo(key) != 0) {
+            return;
+        }
+
+        for (int j = i; j < N-1; j++)  {
+            keys[j] = keys[j+1];
+            values[j] = values[j+1];
+        }
+
+        N--;
+        keys[N] = null;  // to avoid loitering
+        values[N] = null;
+
+        // resize if 1/4 full
+        if (N > 0 && N == keys.length/4) resize(keys.length/2);
+
+    }
+
+    private void resize(int capacity) {
+        assert capacity >= N;
+        Key[] tempk = (Key[]) new Comparable[capacity];
+        Value[] tempv = (Value[]) new Object[capacity];
+        for (int i = 0; i < N; i++) {
+            tempk[i] = keys[i];
+            tempv[i] = values[i];
+        }
+        values = tempv;
+        keys = tempk;
+    }
 }
